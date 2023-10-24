74d3634b9db1354f2bc2ed78f892fcaed387e434
Mahdishehady <shehadi2000@gmail.com>
2023-10-24 15:10:21 +0300

    resolved the issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 "use client"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 
 
 export default function Main() {
 
   const router=useRouter()
-  router.push("/login")
+  useEffect(() => {
+    // Navigate to the "/login" route when the component mounts
+    router.push("/login");
+  }, []);
+  
   return(
     <>
-    .</>
+    </>
   )
    
 }
