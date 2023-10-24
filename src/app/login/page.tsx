5303f63d7b11ff0199413651b05086f734a0f040
Mahdishehady <shehadi2000@gmail.com>
2023-10-24 15:23:32 +0300

    done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,44 +14,28 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      
+
       <div className="container relative hidden h-[100vh] flex-col  items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        {/* <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link> */}
+
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-black-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-           
-            CognativeX
+          <div className="relative z-20 flex justify-center items-center text-4xl font-semibold h-[100%] ">
+
+            CognativeX-DemoWebsite
           </div>
-          {/* <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div> */}
+         
+         
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-               Login
+                Login
               </h1>
-              
+
             </div>
             <UserAuthForm />
-            
+
           </div>
         </div>
       </div>
